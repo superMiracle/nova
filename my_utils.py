705627445691025d50beca73328c42080eca8f8c
urdof7 <urdof7@gmail.com>
2025-07-08 13:25:03 -0400

    move version number

--- my_utils.py
+++ my_utils.py
@@ -422,8 +422,7 @@ def monitor_validator(score_dict, metagraph, current_epoch, current_block, valid
     try:
         import torch
         machine_info = {
-            "torch_version": torch.__version__,
-            "validator_version": 1.1
+            "torch_version": torch.__version__
         }
         if torch.cuda.is_available():
             machine_info["cuda_version"] = torch.version.cuda
@@ -448,6 +447,7 @@ def monitor_validator(score_dict, metagraph, current_epoch, current_block, valid
             "current_block": current_block,
             "blocks_into_epoch": current_block % 361,
             "validator_hotkey": validator_hotkey,
+            "validator_version": 1.1,
             "winning_group": winning_group,
             "machine_info": machine_info
         }, headers={"Authorization": f"Bearer {api_key}"}, timeout=5)
